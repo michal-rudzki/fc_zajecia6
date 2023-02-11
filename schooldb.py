d8ff59c8da2c7b8992db20c6282985aa2c8ea19f
Michał Rudzki <michal@mailx.com.pl>
2023-02-11 20:41:52 +0100

    main body of the program

--- schooldb.py
+++ schooldb.py
@@ -1,4 +1,6 @@
 import sys
+from globals_modules import USER_TYPE
+from class_modules import School
 
 def main():
     while True:
@@ -12,13 +14,20 @@ def main():
             print("[u]czeń, [n]auczyciel, [w]ychowawca")
             user_input = input().strip()
             if user_input in ['u', 'uczeń']:
-                print("Podaj Imie i Nazwisko:")
+                print(f"Podaj Imie i Nazwisko: [{USER_TYPE[0]}] ")
                 student = input().split(' ')
                 print("Podaj nazwę klasy: ")
                 student.append(input().strip())
-                print(student)
+                user_obj = School(None, student[0], student[1], USER_TYPE[0], student[2], None)
+                user_obj.add_user()
             elif user_input in ['n', 'nauczyciel']:
-                pass
+                print(f"Podaj Tytuł oraz Imię i Nazwisko [{USER_TYPE[1]}]: ")
+                teacher = input().split(' ')
+                print(f"Podaj przedmiot(y) jaki będzię uczył: ")
+                print(f"(przedmioty odziel przecinkami)")
+                teacher.append(input().split(','))
+                user_obj = School(teacher[0], teacher[1], teacher[2], USER_TYPE[1], None, teacher[3])
+                user_obj.add_user()
             elif user_input in ['w', 'wychowawca']:
                 pass
             
